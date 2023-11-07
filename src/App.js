bd0348e6aee6bffede1b84640ab352a3e2f4a83b
Franck NGONG <franck.ngong@lapiscine.pro>
2023-11-07 12:47:43 +0100

    exo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Footer from './Footer';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello la piscine</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Dans mon monde a moi il n'ya que des poneys, ils mangent des arc-en-ciel et ils font des cacas papillons
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Clique pour avoir un poney
-        </a>
-      </header>
+    <div>
+      <Header/>,
+    <Footer/>
     </div>
+    
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+    return(<header>
+        <h1>Mon Poney</h1>
+        <nav> 
+            <ul>
+                <li>Menu1</li>
+                <li>Menu2</li>
+                <li>Menu3</li>
+            </ul>
+        </nav>
+    </header>)
+}
+
+export default Header;
